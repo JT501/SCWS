4d90a8ea8586866fcdf309e4f39a568f61c5bb4f
Johnny Choi <hallelujahbaby@gmail.com>
2019-01-29 17:55:22 +0800

    Add 'add_dict' function

--- src/Latrell/Scws/Scws.php
+++ src/Latrell/Scws/Scws.php
@@ -1,24 +1,29 @@
 <?php
+
 namespace Latrell\Scws;
 
 class Scws
 {
 
-	protected $scws;
+    protected $scws;
 
-	public function __construct($config)
-	{
-		$this->scws = \scws_new();
-		foreach ($config as $key => $value) {
-			$this->scws->{'set_' . $key}($value);
-		}
-	}
+    public function __construct($config)
+    {
+        $this->scws = \scws_new();
+        foreach ($config as $key => $value) {
+            if ($key == 'add_dict') {
+                $this->scws->{'add_dict'}($value); //Add dict to SCWS Engine
+            } else {
+                $this->scws->{'set_' . $key}($value);
+            }
+        }
+    }
 
-	public function __call($name, $arguments)
-	{
-		return call_user_func_array([
-			$this->scws,
-			snake_case($name)
-		], $arguments);
-	}
+    public function __call($name, $arguments)
+    {
+        return call_user_func_array([
+            $this->scws,
+            snake_case($name)
+        ], $arguments);
+    }
 }
